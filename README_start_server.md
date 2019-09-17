561e04f6d8510602653b484ebd5559a8283d1087
Olivaluno <oliv.aluno@gmail.com>
2019-09-17 06:35:21 +0200

    Change Asset and Rename Files

---
+++ README_start_server.md
@@ -0,0 +1,5 @@
+# AUTOMATIC OFFLINE SERVER
+
+Drag and Drop the start server file into terminal
+type into any browser http://localhost:8000
+
---
+++ start_server.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /Users/oliveralunovic/Dropbox/Universität/studio\ sejima/WS_2018\:19/Special\ Topics\ in\ Architecture/03_Plans\ and\ Visualization/04_Augumented\ Reality/Publication\ POP\ UP\ Model/02_Github\ Webpage\ Files/Terminale/docs/
+
+python -m SimpleHTTPServer 8000
